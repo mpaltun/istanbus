496d7d16f815ec43a26819716f69dc537f9ab83f
Mustafa Paltun <mpaltun@gmail.com>
2012-02-29 23:38:52 +0200

    set up istanbus_core

---
+++ apps/istanbus_core/src/istanbus_core.erl
@@ -0,0 +1,30 @@
+-module(istanbus_core).
+-author('Mustafa <mpaltun@gmail.com>').
+-export([start/0, start_link/0, stop/0]).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+%% @spec start_link() -> {ok,Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+    ensure_started(crypto),
+    istanbus_core_sup:start_link().
+
+%% @spec start() -> ok
+%% @doc Start the csd_core server.
+start() ->
+    ensure_started(crypto),
+    application:start(istanbus_core).
+
+%% @spec stop() -> ok
+%% @doc Stop the csd_core server.
+stop() ->
+    Res = application:stop(istanbus_core),
+    application:stop(crypto),
+    Res.
