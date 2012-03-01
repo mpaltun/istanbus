6883f04c1e1de8f6d7c85ff0668e81ace9801541
Mustafa Paltun <mpaltun@gmail.com>
2012-03-02 01:19:28 +0200

    working resource with url /bus/id

---
+++ apps/istanbus_web/src/istanbus_web_bus_resource.erl
@@ -0,0 +1,22 @@
+%% @author Mustafa Paltun <mpaltun@gmail.com>
+%% @copyright 2012 mpaltun.
+%% @doc istanbus webmachine_resource.
+
+-module(istanbus_web_bus_resource).
+-export([init/1, to_json/2, content_types_provided/2]).
+
+-include_lib("webmachine/include/webmachine.hrl").
+
+init([]) -> 
+    application:start(emongo),
+    emongo:add_pool(pool_mongo, "localhost", 27017, "istanbus_2012-02-23", 1),
+    {ok, undefined}.
+
+content_types_provided(ReqData, Context) ->
+   {[{"application/json",to_json}], ReqData, Context}.
+
+to_json(ReqData, Context) ->
+    PathInfo = wrq:path_info(ReqData),
+    {ok, BusId} = dict:find(key, PathInfo),
+    Result = emongo:find(pool_mongo, "bus", [{"_id", BusId}]),
+    {mochijson2:encode(Result),  ReqData, Context}.
