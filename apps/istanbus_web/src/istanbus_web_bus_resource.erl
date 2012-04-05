f12b9f0907e4720fa47d190d8a8b25a3ee96f358
Mustafa Paltun <mpaltun@gmail.com>
2012-04-05 18:36:05 +0300

    trust mongo, caching has no acts on performance so I remove it

--- apps/istanbus_web/src/istanbus_web_bus_resource.erl
+++ apps/istanbus_web/src/istanbus_web_bus_resource.erl
@@ -16,43 +16,7 @@ content_types_provided(ReqData, Context) ->
 to_json(ReqData, Context) ->
     case wrq:path_info(id, ReqData) of
         BusId ->
-			Cache = get_the_cache(),
-            DecodedBusId = unicode:characters_to_list(list_to_binary(http_uri:decode(BusId)), utf8),
-            Cache ! {get, DecodedBusId, self()},
-            receive
-                error ->
-                    Bus = istanbus_core_bus_module:load_by_id(DecodedBusId),
-                    Cache = get_the_cache(),
-                    Cache ! {put, DecodedBusId, Bus},
-                    {mochijson2:encode(Bus), ReqData, Context};
-                {ok, Bus} ->
-                    {mochijson2:encode(Bus), ReqData, Context}
-            end
-    end.
-
-cache(Dict) ->
-    receive
-        {get, Key, Pid} ->
-            Pid ! dict:find(Key, Dict),
-            cache(Dict);
-        {put, Key, Value} ->
-            Dict1 = dict:store(Key, Value, Dict),
-            cache(Dict1)
-    end.
-
-get_the_cache() ->
-    % does the cache exists?
-    Pid = whereis(cache_pid),
-    if
-        is_pid(Pid) ->
-            % found
-            Pid;
-        true ->
-            erlang:display("cache started"),
-            % not found, create it
-            NewPid = spawn(fun() ->
-                cache(dict:new())
-            end),
-            register(cache_pid, NewPid),
-            NewPid
+			DecodedBusId = unicode:characters_to_list(list_to_binary(http_uri:decode(BusId)), utf8),
+            Bus = istanbus_core_bus_module:load_by_id(DecodedBusId),
+            {mochijson2:encode(Bus), ReqData, Context}
     end.
