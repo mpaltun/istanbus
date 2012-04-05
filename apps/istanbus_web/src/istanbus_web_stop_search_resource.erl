0df0f6b274f4dba94e1c329fe52862b69a0ebab6
Mustafa Paltun <mpaltun@gmail.com>
2012-04-06 00:35:46 +0300

    stop id with turkish char problem is fixed. search and loadbyid seperated to different modules

--- apps/istanbus_web/src/istanbus_web_stop_search_resource.erl
+++ apps/istanbus_web/src/istanbus_web_stop_search_resource.erl
@@ -2,7 +2,7 @@
 %% @copyright 2012 mpaltun.
 %% @doc istanbus webmachine_resource.
 
--module(istanbus_web_stop_resource).
+-module(istanbus_web_stop_search_resource).
 -export([init/1, to_json/2, content_types_provided/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
@@ -14,7 +14,7 @@ content_types_provided(ReqData, Context) ->
    {[{"application/json",to_json}], ReqData, Context}.
 
 to_json(ReqData, Context) ->
-    StopId = wrq:path_info(id, ReqData),
-    DecodedStopId = unicode:characters_to_list(list_to_binary(http_uri:decode(StopId)), utf8),
-    Stop = istanbus_core_stop_module:load_by_id(DecodedStopId),
-    {mochijson2:encode(Stop),  ReqData, Context}.
+    Keyword = wrq:path_info(keyword, ReqData),
+    DecodedKeyword = unicode:characters_to_list(list_to_binary(http_uri:decode(Keyword)), utf8),
+    Stops = istanbus_core_stop_module:search(DecodedKeyword),
+    {mochijson2:encode(Stops),  ReqData, Context}.
