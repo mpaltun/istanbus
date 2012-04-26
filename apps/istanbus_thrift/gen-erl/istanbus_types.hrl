7595eaa993add3cab63cbb6a160d7b1aa810f834
Mustafa Paltun <mpaltun@gmail.com>
2012-04-27 01:17:17 +0300

    howtogo(recommendation) module done test url : http://localhost:8000/howtogo/from/L0007/to/L0310

---
+++ apps/istanbus_thrift/gen-erl/istanbus_types.hrl
@@ -0,0 +1,4 @@
+-ifndef(_istanbus_types_included).
+-define(_istanbus_types_included, yeah).
+
+-endif.
---
+++ apps/istanbus_thrift/src/istanbus_thrift.erl
@@ -0,0 +1,36 @@
+%% @author Mustafa Paltun <mpaltun@gmail.com>
+%% @copyright 2012 Mustafa Paltun
+
+%% @doc istanbus_thrift init here
+
+-module(istanbus_thrift).
+-author('Mustafa Paltun <mpaltun@gmail.com>').
+-export([start/0, start_link/0, stop/0]).
+
+ensure_started(App) ->
+    case application:start(App) of
+        ok ->
+            ok;
+        {error, {already_started, App}} ->
+            ok
+    end.
+
+%% @spec start_link() -> {ok,Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+    istanbus_thrift_sup:start_link().
+
+%% @spec start() -> ok
+%% @doc Start the istanbus_thrift.
+start() ->
+    {ok, Client} = thrift_client_util:new("127.0.0.1",
+                                           9090,
+                                           recommendationService_thrift,
+                                           []),
+    application:start(istanbus_thrift).
+
+%% @spec stop() -> ok
+%% @doc Stop the istanbus_thrift.
+stop() ->
+    Res = application:stop(istanbus_thrift),
+    Res.
