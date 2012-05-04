af4a86e60d7d7ed55fb8ebdf331c717ad4f3ff5a
Mustafa Paltun <mpaltun@gmail.com>
2012-05-04 18:50:48 +0300

    read from db if recommendation exists

--- apps/istanbus_core/src/istanbus_thrift_bridge.erl
+++ apps/istanbus_core/src/istanbus_thrift_bridge.erl
@@ -4,13 +4,16 @@
 
 recommend(From, To) ->
     {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift, []),
-    Result = emongo:find_one(pool_mongo, "howtogo", [{"_id", From ++ "_" ++ To}]),
-    erlang:display(Result),
-    {Client2, {ok, Resp}} = thrift_client:call(Client, recommend, [From, To]),
-    %Result = {struct, [{from, list_to_binary(From)}, {to, list_to_binary(To)}]},
-    {_, ok} = thrift_client:close(Client2),
-    Recommendations = emongo:find_one(pool_mongo, "howtogo", [{"_id", From ++ "_" ++ To}]),
-    get_first(Recommendations).
+    Key = From ++ "_" ++ To,
+    Result = emongo:find_one(pool_mongo, "howtogo", [{"_id", Key}]),
+    case Result of
+        [] ->
+            {Client2, {ok, _Resp}} = thrift_client:call(Client, recommend, [From, To]),
+            {_, ok} = thrift_client:close(Client2),
+            get_first(emongo:find_one(pool_mongo, "howtogo", [{"_id", Key}]));
+        _ ->
+            get_first(Result)
+    end.
 
 get_closest_stops(Latitude, Longitude) ->
     {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift,[]),
