f54b5d9a67689e5d2aeef35d9e2cfb595ff24bef
Mustafa Paltun <mpaltun@gmail.com>
2012-05-04 15:44:32 +0000

    fix pattern match

--- apps/istanbus_core/src/istanbus_thrift_bridge.erl
+++ apps/istanbus_core/src/istanbus_thrift_bridge.erl
@@ -4,14 +4,22 @@
 
 recommend(From, To) ->
     {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift, []),
-    {Client2, {ok, "ok"}} = thrift_client:call(Client, recommend, [From, To]),
+    Result = emongo:find_one(pool_mongo, "howtogo", [{"_id", From ++ "_" ++ To}]),
+    erlang:display(Result),
+    {Client2, {ok, Resp}} = thrift_client:call(Client, recommend, [From, To]),
     %Result = {struct, [{from, list_to_binary(From)}, {to, list_to_binary(To)}]},
     {_, ok} = thrift_client:close(Client2),
     Recommendations = emongo:find_one(pool_mongo, "howtogo", [{"_id", From ++ "_" ++ To}]),
-    Recommendations.
+    get_first(Recommendations).
 
 get_closest_stops(Latitude, Longitude) ->
     {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift,[]),
     {Client2, {ok, Response}} = thrift_client:call(Client, get_closest_stops, [Latitude, Longitude]),
     {_, ok} = thrift_client:close(Client2),
     Response.
+
+% internal api
+get_first([H | _]) ->
+    H;
+get_first([]) ->
+    {struct, []}.
