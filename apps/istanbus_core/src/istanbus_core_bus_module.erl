d5727eeb8a9c3abcb8dfa112fe331fa81e5188b0
Mustafa Paltun <mpaltun@gmail.com>
2012-06-12 08:40:04 +0000

    bus search

--- apps/istanbus_core/src/istanbus_core_bus_module.erl
+++ apps/istanbus_core/src/istanbus_core_bus_module.erl
@@ -1,6 +1,6 @@
 -module(istanbus_core_bus_module).
 
--export([load_all/0, load_by_id/1]).
+-export([load_all/0, load_by_id/1, search/1]).
 
 load_all() ->
     Result = load_by_id("all"),
@@ -10,7 +10,8 @@ load_all() ->
 load_by_id(BusId) ->
     Result = emongo:find_one(pool_mongo, "bus", [{"_id", BusId}], [{fields, ["stops_go", "time", "stops_come", "notes"]}]),
     get_first(Result).
-
+search(Keyword) ->
+    emongo:find(pool_mongo, "bus", [{"_id",{regexp, [ $^|Keyword], []}}], [{limit, 20}, {fields,     ["name"]}]).
 % internal api
 get_first([H | _]) ->
     H;
