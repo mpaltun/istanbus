a70f7225ae6e04de1ee29c0d4d5ba14d5b969f53
challenge <mpaltun@gmail.com>
2012-06-18 13:40:14 +0300

    search by multiple keywords for stops i.e: /stop/search/kadir has

--- apps/istanbus_core/src/istanbus_core_stop_module.erl
+++ apps/istanbus_core/src/istanbus_core_stop_module.erl
@@ -4,11 +4,20 @@
 
 load_by_id(StopId)  ->
     get_first(emongo:find_one(pool_mongo, "stop", [{"_id", StopId}], [{fields, ["name", "bus_list"]}])).
-search(Keyword)     ->
-    emongo:find(pool_mongo, "stop", [{"words",{regexp, [ $^|Keyword], []}}], [{limit, 20}, {fields, ["name"]}]).
+search(Keywords)     ->
+    Query = prepare_query(Keywords, []),
+    emongo:find(pool_mongo, "stop", Query, [{limit, 20}, {fields, ["name"]}]).
 
 % internal api
 get_first([H | _]) ->
     H;
 get_first([]) ->
     {struct, []}.
+
+
+prepare_query([], Queries) ->
+    Queries;
+
+prepare_query([Keyword | Keywords], Queries) ->
+    Query = {"words",{regexp, [$^ | Keyword], []}},
+    prepare_query(Keywords, [Query | Queries]).
