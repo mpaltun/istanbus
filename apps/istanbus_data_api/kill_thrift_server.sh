44a1e95e1e9452545b9d1563e318d3702f112e88
Mustafa Paltun <mpaltun@gmail.com>
2012-07-07 23:34:10 +0300

    change directory structure

--- apps/istanbus_data_api/kill_thrift_server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ps aux | grep "python ./istanbus_thrift_server.py" | awk '{print $2}' | xargs kill -9
--- apps/istanbus_data_api/producestophtml.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl --header "Referer: http://harita.iett.gov.tr/" -d "DurakName=.&ara=DETAY" "http://mobil.iett.gov.tr/durakarama.php" > output/stop.html
