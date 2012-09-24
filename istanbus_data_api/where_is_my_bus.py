32c28f496df25024294be9f591e025d8eaeee143
Mustafa Paltun <mpaltun@gmail.com>
2012-09-24 22:18:45 +0000

    improvement

--- istanbus_data_api/where_is_my_bus.py
+++ istanbus_data_api/where_is_my_bus.py
@@ -6,16 +6,18 @@ import re
 class Where_is_my_bus:
     def __init__(self):
         self.client = Client("3n.iett.gov.tr:80")
-        self.headers = {"Referer" : "http://3n.iett.gov.tr/", "User-Agent" : "curl/7.22.0 (i686-pc-linux-gnu) libcurl/7.22.0 OpenSSL/1.0.1 zlib/1.2.3.4 libidn/1.23 librtmp/2.3"}
+        self.headers = {"Referer" : "http://3n.iett.gov.tr/", "User-Agent" : "Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us) AppleWebKit/528.18 (KHTML, like Gecko) Version/4.0 Mobile/7A341 Safari/528.16"}
         self.params = "hat={}&sira={}"
         self.url = '/?'
 
     def find(self, bus_id, order=""):
         response = self.client.get(self.url + self.params.format(bus_id, order), "" , self.headers)
         parsed_xml = lxml.html.parse(response, lxml.html.HTMLParser(encoding="utf-8"))
-        info_list = parsed_xml.xpath('//*[contains(text(),"otob")]/text()')
+        info_list = parsed_xml.xpath("/html/body//text()")
         for info in info_list:
-          print info
+            result = re.search("^(.*) numaral. otob.s (.*) olup (.*) saniye .nce (.*) y.n.nde (.*) s.ra nolu (.*) dura..(.*)", info)
+            if result is not None:
+                print result.group(7) 
 
 module = Where_is_my_bus()
-module.find('103');
+module.find('110');
