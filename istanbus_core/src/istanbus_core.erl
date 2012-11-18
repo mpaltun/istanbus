90b0971fc0d1d3840692737da90936d3f4416820
Mustafa Paltun <mpaltun@gmail.com>
2012-11-18 13:13:31 +0200

    query closest stops from mongo

--- istanbus_core/src/istanbus_core.erl
+++ istanbus_core/src/istanbus_core.erl
@@ -27,7 +27,7 @@ start_link() ->
 start() ->
     ensure_started(crypto),
     ensure_started(emongo),
-    emongo:add_pool(pool_mongo, "localhost", 27017, "istanbus_2012-08-15", 10),
+    emongo:add_pool(pool_mongo, "localhost", 27017, "istanbus_2012-11-18", 10),
     application:start(istanbus_core).
 
 %% @spec stop() -> ok
