feb5675d4514f51b2ea18c0cf74a23061f8ef865
Mustafa Paltun <mpaltun@gmail.com>
2012-12-23 23:37:04 +0200

    merge projects

--- istanbus_core/src/istanbus_thrift_bridge.erl
+++ istanbus_core/src/istanbus_thrift_bridge.erl
@@ -1,29 +1,17 @@
 -module(istanbus_thrift_bridge).
 
--export([recommend/2, where_is_my_bus/1]).
+-export([recommend/2, stop_search/1]).
 
 recommend(From, To) ->
-    Key = From ++ "_" ++ To,
-    DecodedKey = unicode:characters_to_list(list_to_binary(http_uri:decode(Key)), utf8),
-    Result = emongo:find_one(pool_mongo, "howtogo", [{"id", DecodedKey}], [{fieldsnoid, ["id", "recommendations"]}]),
-    case Result of
-        [] ->
-            {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift, []),
-            {Client2, {ok, _Resp}} = thrift_client:call(Client, recommend, [From, To]),
-            {_, ok} = thrift_client:close(Client2),
-            get_first(emongo:find_one(pool_mongo, "howtogo", [{"id", DecodedKey}], [{fieldsnoid, ["id", "recommendations"]}]));
-        _ ->
-            get_first(Result)
-    end.
+  Opts = [{strict_read, false}, {strict_write, false}, {framed, true}],
+  {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusJavaService_thrift, Opts),
+  {Client2, {ok, Response}} = thrift_client:call(Client, recommend, [From, To]),
+  {_, ok} = thrift_client:close(Client2),
+  Response.
 
-where_is_my_bus(BusName) ->
-    {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusService_thrift,[]),
-    {Client2, {ok, Response}} = thrift_client:call(Client, where_is_my_bus, [BusName]),
-    {_, ok} = thrift_client:close(Client2),
-    Response.
-
-% internal api
-get_first([H | _]) ->
-    H;
-get_first([]) ->
-    {struct, []}.
+stop_search(Keyword) ->
+  Opts = [{strict_read, false}, {strict_write, false}, {framed, true}],
+  {ok, Client} = thrift_client_util:new("127.0.0.1", 9090, istanbusJavaService_thrift, Opts),
+  {Client2, {ok, Response}} = thrift_client:call(Client, stop_search, [Keyword]),
+  {_, ok} = thrift_client:close(Client2),
+  Response.
