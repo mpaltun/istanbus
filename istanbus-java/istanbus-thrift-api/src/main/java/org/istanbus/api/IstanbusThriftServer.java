174880d9e209d227bd24457816f78a74db6375b1
Mustafa Paltun <mpaltun@gmail.com>
2013-01-05 16:13:06 +0200

    improve java thrift server performance

--- istanbus-java/istanbus-thrift-api/src/main/java/org/istanbus/api/IstanbusThriftServer.java
+++ istanbus-java/istanbus-thrift-api/src/main/java/org/istanbus/api/IstanbusThriftServer.java
@@ -1,14 +1,11 @@
 package org.istanbus.api;
 
 import com.google.inject.Inject;
-import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TNonblockingServerTransport;
 import org.apache.thrift.transport.TTransportException;
-import org.istanbus.core.service.PathFinderService;
-import org.istanbus.core.service.SearchService;
-import org.istanbus.core.service.impl.SearchServiceImpl;
 
 public class IstanbusThriftServer {
 
@@ -20,12 +17,17 @@ public class IstanbusThriftServer {
     }
 
     public void start() {
+        IstanbusJavaService.Processor processor = new IstanbusJavaService.Processor(istanbusJavaService);
+
         try {
             TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(9090);
-            IstanbusJavaService.Processor processor = new IstanbusJavaService.Processor(istanbusJavaService);
 
-            TServer server = new TNonblockingServer(new TNonblockingServer.Args(serverTransport).
-                    processor(processor));
+            THsHaServer.Args args = new THsHaServer.Args(serverTransport);
+            args.workerThreads(5);
+            args.processor(processor);
+
+            TServer server = new THsHaServer(args);
+
             System.out.println("Starting server on port 9090 ...");
             server.serve();
         } catch (TTransportException e) {
