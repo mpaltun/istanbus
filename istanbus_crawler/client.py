4428310d79825eeea0feb8fa119709c4f86e574a
Mustafa Paltun <mpaltun@gmail.com>
2013-01-05 14:08:59 +0200

    exception handling on get request

--- istanbus_crawler/client.py
+++ istanbus_crawler/client.py
@@ -3,18 +3,25 @@
 import httplib
 
 class Client:
-	def __init__(self, url):
-		self.url = url
+    def __init__(self, url):
+        self.url = url
 
-	def get(self, sub_url, params, headers):
-		connection = httplib.HTTPConnection(self.url)
-		connection.request("GET", sub_url, params, headers)
-		return connection.getresponse()
+    def get(self, sub_url, params, headers):
+        try:
+            return self.get_request(sub_url, params, headers)
+        except Exception:
+            #try once again
+            return self.get_request(sub_url, params, headers)
 
-	def post(self, sub_url, params, headers):
-		connection = httplib.HTTPConnection(self.url)
-		connection.request("POST", sub_url, params, headers)
-		return connection.getresponse()
+    def get_request(self, sub_url, params, headers):
+        connection = httplib.HTTPConnection(self.url)
+        connection.request("GET", sub_url, params, headers)
+        return connection.getresponse()
 
-	def close(self):
-		pass
+    def post(self, sub_url, params, headers):
+        connection = httplib.HTTPConnection(self.url)
+        connection.request("POST", sub_url, params, headers)
+        return connection.getresponse()
+
+    def close(self):
+        pass
