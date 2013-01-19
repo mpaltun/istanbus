07a5315b15e39c7cba004ac3988b693a303c313d
Mustafa Paltun <mpaltun@gmail.com>
2013-01-20 01:02:12 +0200

    - fix OOM - take thrift server to localhost

--- istanbus-java/istanbus-thrift-api/src/main/java/org/istanbus/api/IstanbusThriftServer.java
+++ istanbus-java/istanbus-thrift-api/src/main/java/org/istanbus/api/IstanbusThriftServer.java
@@ -6,10 +6,16 @@ import org.apache.thrift.server.TServer;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TNonblockingServerTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
 
 public class IstanbusThriftServer {
 
     private IstanbusJavaService.Iface istanbusJavaService;
+
+    private static final Logger logger = LoggerFactory.getLogger(IstanbusThriftServer.class);
 
     @Inject
     public IstanbusThriftServer(IstanbusJavaService.Iface istanbusJavaService) {
@@ -19,17 +25,22 @@ public class IstanbusThriftServer {
     public void start() {
         IstanbusJavaService.Processor processor = new IstanbusJavaService.Processor(istanbusJavaService);
 
+        String host = "127.0.0.1";
+        int port = 9090;
+
         try {
-            TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(9090);
+            InetSocketAddress address = new InetSocketAddress(host, port);
+            TNonblockingServerTransport serverTransport = new TNonblockingServerSocket(address);
 
             THsHaServer.Args args = new THsHaServer.Args(serverTransport);
+            args.maxReadBufferBytes = 5242880L;
             args.workerThreads(5);
             args.processor(processor);
 
             TServer server = new THsHaServer(args);
-
-            System.out.println("Starting server on port 9090 ...");
             server.serve();
+
+            logger.info("Started server at {}:{} ...", host, port);
         } catch (TTransportException e) {
             e.printStackTrace();
         }
