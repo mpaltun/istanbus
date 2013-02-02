a8e51e73e157b5ff2c06ec11cbaa1702dbb16a1e
mp <mpaltun@gmail.com>
2013-02-02 23:35:54 +0200

    - set bus list of a stop offline

--- istanbus_crawler/mongo_stop_insert_query.js
+++ istanbus_crawler/mongo_stop_insert_query.js
@@ -11,3 +11,14 @@ db.stop2.find().forEach(function (stop2) {
 });
 db.stop.ensureIndex( { location : "2d" } );
 db.stop2.drop();
+
+db.stop.find().forEach(function (stop) {
+	var bus_list = db.bus.find({ $or: [ { "stops.turn.id" : stop.id }, { "stops.go.id" : stop.id } ]},
+		{id : 1, _id : 0, name : 1}).toArray();
+	stop.bus = [];
+	for (var i = 0; i < bus_list.length; i++) {
+		var bus = bus_list[i];
+		stop.bus.push(bus);
+	};
+	db.stop.update({_id : stop._id}, stop);
+});
