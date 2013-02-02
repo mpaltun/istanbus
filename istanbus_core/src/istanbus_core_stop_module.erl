3ea3776040599c383d889c2af0a2be3699f26d9f
mp <mpaltun@gmail.com>
2013-02-02 23:36:49 +0200

    load stops at one query

--- istanbus_core/src/istanbus_core_stop_module.erl
+++ istanbus_core/src/istanbus_core_stop_module.erl
@@ -3,9 +3,8 @@
 -export([load_by_id/1, search/1, find_closest/2]).
 
 load_by_id(StopId)  ->
-    BusList = istanbus_core_bus_module:load_by_stop(StopId),
-    Stop = get_first(emongo:find_one(pool_mongo, "stop", [{"id", StopId}], [{fieldsnoid, ["id", "name", "location"]}])),
-    [{ <<"bus_list">>,  BusList } | Stop].
+    Stop = get_first(emongo:find_one(pool_mongo, "stop", [{"id", StopId}], [{fieldsnoid, ["id", "name", "location", "bus"]}])),
+    Stop.
     
 
 search(Keywords)     ->
